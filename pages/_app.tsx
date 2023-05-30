12a253a141585e8bd56f8aa736dfcc4fc529de3d
Harry Bairstow <me@harryet.xyz>
2023-05-30 22:51:26 +0100

    feat: og card

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,27 @@ export default function App({ Component, pageProps }: AppProps) {
       <DefaultSeo
         title='CatPowered Ltd'
         description='Building software that scales.'
+        canonical='https://catpowered.xyz'
+        themeColor='#748B75'
+        twitter={{
+          site: '@catpowered_',
+          cardType: 'summary_large_image',
+          handle: '@catpowered_',
+        }}
+        openGraph={{
+          type: 'website',
+          url: 'https://catpowered.xyz',
+          title: 'CatPowered Ltd',
+          description: 'Building software that scales.',
+          images: [
+            {
+              url: 'https://catpowered.xyz/og-image.png',
+              width: 800,
+              height: 600,
+              alt: 'CatPowered Ltd OG Image',
+            },
+          ],
+        }}
         additionalLinkTags={[
           {
             rel: 'icon',
